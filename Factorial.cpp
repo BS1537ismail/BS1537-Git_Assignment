3f1abee5b0b371b76536a7cc6f76003d804da53a
Ismail <md.ismail@brainstation-23.com>
2024-04-25 12:29:42 +0600

    this is factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a non-negative integer: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Error: Factorial is undefined for negative numbers." << endl;
+        return 1;
+    }
+
+    unsigned long long result = factorial(num);
+    cout << "Factorial of " << num << " is: " << result << endl;
+
+    return 0;
+}
